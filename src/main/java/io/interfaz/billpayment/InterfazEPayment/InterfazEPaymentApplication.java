262acaacf8dc2a4564f04d0fe765b787b41c9735
ivanfcomesen <ivanfcomesen@gmail.com>
2018-08-27 17:59:46 -0600

    Interfaz-E-Payment

---
+++ src/main/java/io/interfaz/billpayment/InterfazEPayment/InterfazEPaymentApplication.java
@@ -0,0 +1,12 @@
+package io.interfaz.billpayment.InterfazEPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfazEPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfazEPaymentApplication.class, args);
+	}
+}
